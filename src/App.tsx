5517d2c425254480d4ce0bf90bbcfb757875ba40
93amr.kakashi <93amr.kakashi@gmail.com>
2023-04-11 19:25:22 +0700

    added some syyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from "react";
+import { Form } from "react-bootstrap";
+import Button from "react-bootstrap/esm/Button";
+import CloseButton from "react-bootstrap/esm/CloseButton";
+import Navbar from "./components/Navbar";
 interface ITask {
   taskName: string;
-  deadline: number;
 }
 
 function App() {
-  const [task, setTask] = useState<string>('');
-  const [deadline, setDealine] = useState<number>(0);
+  const [task, setTask] = useState<string>("");
+  // const [deadline, setDealine] = useState<number>(0);
   const [todoList, setTodoList] = useState<ITask[]>(() => {
     try {
-      const storedTodos = localStorage.getItem('todos');
+      const storedTodos = localStorage.getItem("todos");
       return storedTodos ? JSON.parse(storedTodos) : [];
     } catch (err) {
       console.error(err);
@@ -18,10 +21,8 @@ function App() {
   });
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    if (event.target.name === 'task') {
+    if (event.target.name === "task") {
       setTask(event.target.value);
-    } else {
-      setDealine(Number(event.target.value));
     }
   };
 
@@ -32,53 +33,65 @@ function App() {
   };
 
   const addTask = (): void => {
-    const newTask = { taskName: task, deadline: deadline };
+    const newTask = { taskName: task };
     setTodoList([...todoList, newTask]);
-    setTask('');
-    setDealine(0);
+    setTask("");
   };
 
   useEffect(() => {
     try {
-      localStorage.setItem('todos', JSON.stringify(todoList));
+      localStorage.setItem("todos", JSON.stringify(todoList));
     } catch (err) {
       console.error(err);
     }
   }, [todoList]);
 
-
-
-  
   return (
     <>
-      {/* <Navbar /> */}
-      <div className="todo-form">
-        <form>
-          <input
+      <Navbar />
+      <div className="todo-form p-2 d-flex flex-column justify-content-center align-items-center">
+        <Form className="d-flex w-75">
+          <Form.Control
+            className="w-100 m-2"
             required
             type="text"
-            placeholder="Task..."
+            placeholder="Write Task Here ..."
             name="task"
+            autoComplete="false"
             value={task}
             onChange={handleChange}
           />
-          <input
+
+          {/* <Form.Control className='w-75 m-2' required
             type="number"
             placeholder="Deadline (in Days)..."
             name="deadline"
             value={deadline}
             onChange={handleChange}
-            required={true}
-          />
-        </form>
-        <button onClick={addTask}>Add Task</button>
+             /> */}
+        </Form>
+        <Button
+          className="w-75"
+          variant="dark"
+          disabled={!task}
+          onClick={addTask}
+        >
+          Add Task
+        </Button>
       </div>
-      <div className="todos">
+      <div className="todos w-95 p-2 d-flex flex-column justify-content-center align-items-center gap-2">
         {todoList?.map((todo) => (
-          <div className="todo" key={Math.random()}>
+          <div
+            className="todo border rounded w-95 text-white bg-secondary"
+            key={Math.random()}
+          >
             <h1>{todo.taskName}</h1>
-            <h3>{todo.deadline}</h3>
-            <button onClick={() => completeTask(todo.taskName)}>X</button>
+            {/* <button className='position-absolute t-2 r-2' >X</button> */}
+            <CloseButton
+              onClick={() => completeTask(todo.taskName)}
+              variant="white"
+              
+            />
           </div>
         ))}
       </div>
